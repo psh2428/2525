f134aa7f2182d3cdb9262e4c435727aa86f96e22
psh2428 <simon2428@naver.com>
2023-03-28 12:02:31 +0900

    초기 커밋.

---
+++ 2525/2525.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+
+void main() {
+	int need;
+
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+
+	printf("%d %d\n", tm.tm_hour, tm.tm_min);
+
+	scanf("%d", &need);
+	if (0 > need || need > 1001) {
+		printf("오류입니다.");
+		return;
+	}
+	else if (0 <= need && need <= 1000) {
+		int min2 = tm.tm_min + need;
+		int hour = tm.tm_hour;
+		if (min2 > 60) {
+			min2 -= 60;
+			hour += 1;
+		}
+		printf("%d %d", hour, min2);
+	}
+	
+}
